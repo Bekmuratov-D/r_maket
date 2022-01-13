595dd2d8c54b28c5e8be43512e7783a42554f4da
Dilmurat <bekmuratdilmurat18@gmail.com>
2022-01-13 07:48:46 +0300

    laba-11

--- js2/app.js
+++ js2/app.js
@@ -58,7 +58,55 @@ let student = {
     "last name": "Бекмуратов",
     "first name": "Дилмуратов",
 };
-console.log('Студент ' + student["first name"] + student["last name"] + ' учится в ' + student.group + ' группе')
+console.log('Студент ' + student["first name"] + student["last name"] + ' учится в ' + student.group + ' группе');
+
+
+
+ // Задача №5 Переключение элементов
+
+let position = 0;
+const slidesToShow = 4;
+const sliderToScroll = 1;
+const container = document.querySelector('.slider-container');
+const track = document.querySelector('.slider-track');
+// const item = document.querySelector('.slider-item');
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+const itemsCount = item.length;
+const items = document.querySelectorAll('.slider-item');
+const itemWidth = items / slidesToShow;
+const movePosition = sliderToScroll * itemWidth;
+
+
+items.forEach((item) => {
+    item.style.minWidth = `${itemWidth}px`;
+});
+
+btnNext.addEventListener('click', () => {
+    const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+    position -= itemsLeft >= sliderToScroll ? movePosition : itemsLeft * itemWidth;
+
+    setPosition();
+    checkBtns();
+});
+
+btnPrev.addEventListener('click', () => {
+    const itemsLeft = Math.abs(position) / itemWidth ;
+    position += itemsLeft >= sliderToScroll ? movePosition : itemsLeft * itemWidth;
+
+    setPosition();
+    checkBtns();
+});
+
+const setPosition = () => {
+    track.style.transform = `translateX(${position}px)`;
+}
+
+const checkBtns = () => {
+    btnPrev.disabled = position === 0;
+    btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+};
+checkBtns();
 
  
 
