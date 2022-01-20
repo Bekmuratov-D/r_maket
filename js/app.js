20d98d70f9834ab6d489745af66c86661e56bfaf
Dilmurat <bekmuratdilmurat18@gmail.com>
2022-01-20 08:58:56 +0300

    1

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 
 console.log("Салат")
-console.log('Предположение:'+"string")
+console.log('Предположение: string')
 console.log('Фактический:', typeof("Салат"))
 
-console.log(9)
+console.log('9')
 console.log('Предположение:', 'number')
 console.log('Фактический:', typeof(9))
 
-console.log(1.2)
+console.log('1.2')
 console.log('Предположение:', 'number')
 console.log('Фактический:', typeof(1.2))
 
-console.log(NaN)
+console.log('NaN')
 console.log('Предположение:', 'number')
 console.log('Фактический:', typeof(NaN))
 
@@ -19,35 +19,39 @@ console.log("Hello World")
 console.log('Предположение:', 'string')
 console.log('Фактический:', typeof("Hello World"))
 
-console.log(true)
+console.log('true')
 console.log('Предположение:', 'boolean')
 console.log('Фактический:', typeof(true))
 
-console.log(2 != 1)
+console.log('2 != 1')
 console.log('Предположение:', 'boolean')
 console.log('Фактический:', typeof(2 != 1))
 
-console.log("сыр" + "ы")
+console.log('"сыр" + "ы"')
 console.log('Предположение:', ' string')
 console.log('Фактический:', typeof("сыр" + "ы"))
 
-console.log("2" + "4")
+console.log('"сыр" - "ы"')
+console.log('Предположение:', ' string')
+console.log('Фактический:', typeof("сыр" - "ы"))
+
+console.log('"2" + "4"')
 console.log('Предположение:', 'string')
 console.log('Фактический:', typeof("2" + "4"))
 
-console.log("2" - "4")
+console.log('"2" - "4"')
 console.log('Предположение:', 'number')
 console.log('Фактический:', typeof("2" - "4"))
 
-console.log("Сэм" + 5)
+console.log('"Сэм" + 5')
 console.log('Предположение:', 'string')
 console.log('Фактический:', typeof("Сэм" + 5))
 
-console.log("Сэм" - 5)
+console.log('"Сэм" - 5')
 console.log('Предположение:', 'number')
 console.log('Фактический:', typeof("Сэм" - 5))
 
-console.log(99 * "шары")
+console.log('99 * "шары"')
 console.log('Предположение:', 'number')
 console.log('Фактический:', typeof(99 * "шары"))
 
@@ -62,11 +66,11 @@ console.log(area)
 console.log(perimeterArea)
 
 //Задача №3 Конвертация единиц измерения
-//В первой вместо дата поставить существующие типы данных, третья - обозначение градуса с C и F, елка должна генерироваться через код
+//елка должна генерироваться через код
 
 console.log("Задача №3")
 let celsius = 25;
-let fahrenheit = 69.2;
+let fahrenheit = 100;
 let convertToFahrenheit = celsius * 1.8 + 32 ;
 let convertToCelsius = (fahrenheit - 32) / 1.8 ;
 
@@ -75,31 +79,33 @@ console.log(`${fahrenheit}F соответствует ${convertToCelsius}°C`)
 
 //Задача №4 Високосный год
 console.log('Задача №4');
-//let year = prompt('Задание №4 Year:');
+let year = prompt('Задание №4 Year:');
 
-// alert(year % 4 == 0 && year % 100 != 0 ? "true" : "false")
+alert(year % 4 == 0 && year % 100 != 0 ? "true" : "false")
 
 //Задание №5 Проверка
 console.log('Задание №5')
-let num1 = 9;
+let num1 = 1;
 let num2 = 10;
 
 // &&-and; || - or; ! - не
-//if (num1 == 10 || num2 == 10 || num1 + num2 == 10) {
-//      console.log(true)
-//} else{
-//    console.log(false)
-//}
+if (num1 == 10 || num2 == 10 || num1 + num2 == 10) {
+      console.log(true)
+} else{
+    console.log(false)
+}
 
 
 // Задача №6 Считаем овец
 console.log('Задача №6')
-//let numSheep = prompt('numSheep');
-//let i = 0;
-//while (i < numSheep){
-//    alert((i+1) + 'овечка...')
- //   i++
-//}
+let numSheep = prompt('numSheep');
+let i = 0;
+let ab = '';
+while (i < numSheep){
+   ab = ab + (`${i+1}  овечка...`)
+  i++
+}
+alert(ab)
 
 //Задача №7 Четное-нечетное
 console.log('Задача №7')
@@ -113,22 +119,23 @@ for (let i =0; i < 16; i++) {
 
 
 //Задача №8 Елка к новому году
-console.log("Задача №8")
+console.log("Задача №8");
 
-function tree(line){
-    let str2 = '';
-    let str1 = '';
-    for(let i = 1; i <= line; i++){
-        if (i % 2 === 0){
-            str2 = str2 + '#'
-            console.log(str2);
-        } else {
-           str1 = str1 + '*';
-           console.log(str1);
-        }
+let num = 12;
+console.log(createChristmasTree(num));
+function createChristmasTree(num) {
+    let result = "";
+    
+    for (let j = 1; j <= num; j++) {
+    if (j % 2 == 1) {
+    result += "*".repeat(j)+ '\n' ;
+    } else {
+    result += "#".repeat(j)+ '\n' ;
     }
+    }
+    
+    return result;
 }
-tree(12);
 
 //Задание № 9 Задача №9 Сортировка
 console.log("Задание № 9")
